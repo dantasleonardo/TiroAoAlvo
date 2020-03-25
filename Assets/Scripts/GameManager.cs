0644eb3d7f3141a3aa9ef08ce5e8830831a92456
dantasleonardo <47763070+dantasleonardo@users.noreply.github.com>
2020-03-25 18:19:59 -0300

    GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int m_pontuacaoAtual;
+
+    public void AdicionarPonto()
+    {
+        m_pontuacaoAtual += 1;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MarcarPonto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarcarPonto : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        FindObjectOfType<GameManager>().AdicionarPonto();
+        Destroy(this.gameObject);
+    }
+}
