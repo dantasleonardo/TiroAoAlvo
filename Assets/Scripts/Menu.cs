d763c80b20f7c79734094a08869faa7d5e141450
dantasleonardo <47763070+dantasleonardo@users.noreply.github.com>
2020-03-29 18:46:35 -0300

    Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Contém os scripts para todas as ações dos botões de menu e pause
+public class Menu : MonoBehaviour
+{
+   public void MenuPrincipal()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Voltar()
+    {
+        FindObjectOfType<GameManager>().Pausar();
+    }
+
+    public void Creditos()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("SAIU");
+        Application.Quit();
+    }
+}
